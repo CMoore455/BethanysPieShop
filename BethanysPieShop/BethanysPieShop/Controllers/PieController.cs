d6ff52054395a9a3c5f5b5a81258ab959056d95e
Christian Moore <christian.g.moore111@gmail.com>
2021-08-10 11:25:28 -0600

    inital commit

---
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+	public class PieController : Controller
+	{
+		private readonly IPieRepository pieRepository;
+		private readonly ICategoryRepository categoryRepository;
+
+		public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+		{
+			this.pieRepository = pieRepository;
+			this.categoryRepository = categoryRepository;
+		}
+
+		public IActionResult List()
+		{
+			PiesListViewModel piesListViewModel = new PiesListViewModel();
+			piesListViewModel.Pies = pieRepository.AllPies;
+
+			piesListViewModel.CurrentCategory = "Chesse Cakes";
+			return View(piesListViewModel);
+		}
+	}
+}
